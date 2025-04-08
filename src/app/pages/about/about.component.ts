2f787faf031df699963f948901fa3a38cb7d58cf
ShroukMagdy3 <shorokmagdy15@gmail.com>
2025-04-08 20:29:07 +0200

    changes

--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+declare let ScrollReveal: any;
 @Component({
   selector: 'app-about',
   imports: [],
@@ -7,5 +7,24 @@ import { Component } from '@angular/core';
   styleUrl: './about.component.css'
 })
 export class AboutComponent {
+ngAfterViewInit(): void {
+  ScrollReveal().reveal('.from-left', {
+    delay: 500,
+    distance: '100px',
+    duration: 1500,
+    origin: 'left',
+    easing: 'ease-in-out',
+    reset: true
+  });
+  ScrollReveal().reveal('.from-right', {
+    delay: 500,
+    distance: '100px',
+    duration: 1500,
+    origin: 'right',
+    easing: 'ease-in-out',
+    reset: true
+  });
 
+
+}
 }
